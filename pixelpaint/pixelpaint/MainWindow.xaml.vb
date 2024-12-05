93e9740469438e2682bb6df2c5ea73ed0d9972bf
Braiden101 <bnbur983@learner.dvusd.org>
2024-12-05 14:02:24 -0700

    Colors named
    Colors named

--- pixelpaint/pixelpaint/MainWindow.xaml.vb
+++ pixelpaint/pixelpaint/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub Fill1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Fill1.MouseDown, FIll2.MouseDown, Fill3.MouseDown, Fill4.MouseDown, Fill5.MouseDown, Fill6.MouseDown, Fill7.MouseDown, Fill8.MouseDown
+        Brush1.Fill = sender.Fill
+    End Sub
 End Class
